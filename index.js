221145d82ce20b8f8c86f4fe85bd3964bb12068b
TravWill228 <travwilliams908@gmail.com>
2022-06-20 19:20:59 -0400

    First game push

--- index.js
+++ index.js
@@ -1,6 +1,44 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+// let greenCharacter = document.createElement('img')
+// greenCharacter.src = 'assets/green-character.gif'
+// greenCharacter.style.position = 'fixed'
+// greenCharacter.style.left = '100px'
+// greenCharacter.style.bottom = '100px'
+// document.body.append(greenCharacter)
+
+// let pineTree = document.createElement('img')
+// pineTree.src = 'assets/pine-tree.png'
+// pineTree.style.position = 'fixed'
+// pineTree.style.left = '450px'
+// pineTree.style.bottom = '200px'
+// document.body.append(pineTree)
+
+function newImage(imgs, left, bottom){
+    let image = document.createElement('img')
+    image.src = imgs;
+    image.style.position = 'fixed'
+    image.style.left = left + 'px'
+    image.style.bottom = bottom + 'px'
+    document.body.append(image)
+    return image;
+}
+
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+
+
+function newItem(img, left, bottom){
+    let item = newImage(img, left, bottom)
+    item.addEventListener('dblclick', () => {
+        item.remove()
+    })
+    return item
+}
+
+newItem('assets/sword.png', 500, 405)
+newItem('assets/sheild.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
+
